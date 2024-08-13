ad2642e82af33dfa0140586df8a9b9a64f64907b
Luke <luke@Luke-iMac-2020.local>
2024-08-14 04:47:51 +1200

    changed ip.py by using psutil

--- ip.py
+++ ip.py
@@ -1,16 +1,17 @@
-import socket
+import psutil
 
-def get_local_ip():
-    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    try:
-        # 连接到一个远程地址，这个连接并不会真正发出请求，只是用于获取本地的 IP 地址
-        s.connect(("8.8.8.8", 80))
-        local_ip = s.getsockname()[0]
-    except Exception:
-        local_ip = "127.0.0.1"
-    finally:
-        s.close()
-    return local_ip
+# 获取所有网络接口的地址信息
+addrs = psutil.net_if_addrs()
 
-print(f"Local IP Address: {get_local_ip()}")
+# 存储所有绑定的 IP 地址
+ip_addresses = set()
+
+for interface, addr_info in addrs.items():
+    for addr in addr_info:
+        if addr.family == socket.AF_INET:  # 只考虑IPv4地址
+            ip_addresses.add(addr.address)
+
+print("All IP addresses bound to the server:")
+for ip in ip_addresses:
+    print(ip)
 
